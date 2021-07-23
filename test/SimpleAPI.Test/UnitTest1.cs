4dbe4351fe90202cef9272e77c6366b347a4878f
Sebastian <Aldo.Sebastian@philips.com>
2021-07-23 17:44:03 +0200

    restore test

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace SimpleAPI.Test
         public void Test1()
         {
             string returnValue = HomeController.Get();
-            Assert.Equal("Les Jack7son", returnValue);
+            Assert.Equal("Les Jackson", returnValue);
         }
     }
 }
